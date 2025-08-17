526bab50d40e2817e96ecf142d586995645c1c44
AdilameerAdi <adilameeradi@gmail.com>
2025-08-17 13:18:32 +0500

    Updated Img component, removed old logos, added new logo

--- src/design/home/Homemain.jsx
+++ src/design/home/Homemain.jsx
@@ -1,8 +1,10 @@
 import Footer from "../Footer"
 import Home from "./home"
 import EventSlider from "./ev"
+import Img from "./img"
 export default function Homemain({ events }){
     return<>
+    <Img></Img>
     <EventSlider events={events}></EventSlider>
     <Home></Home>
     <Footer></Footer>
